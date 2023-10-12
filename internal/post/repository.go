310495379a872afdd26a13900c05640e22fcf8bf
aclgo <marcellosanttos2014@gmail.com>
2023-10-12 17:27:52 -0300

    create method update and findall post

--- internal/post/repository.go
+++ internal/post/repository.go
@@ -2,27 +2,25 @@ package post
 
 import (
 	"context"
-	"time"
 
 	"github.com/filhodanuvem/ytgoapi/internal"
-	"github.com/google/uuid"
-	"github.com/jackc/pgx/v4"
+
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 type Repository interface {
-	Insert(post internal.Post) (internal.Post, error)
-	FindOneByID(id uuid.UUID) (internal.Post, error)
-	Delete(id uuid.UUID) error
+	Insert(ctx context.Context, post internal.Post) (internal.Post, error)
+	FindAll(ctx context.Context) ([]*internal.Post, error)
+	FindOneByID(ctx context.Context, id string) (internal.Post, error)
+	Update(ctx context.Context, post internal.Post) error
+	Delete(ctx context.Context, id string) error
 }
 
 type RepositoryPostgres struct {
 	Conn *pgxpool.Pool
 }
 
-func (r *RepositoryPostgres) Insert(post internal.Post) (internal.Post, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+func (r *RepositoryPostgres) Insert(ctx context.Context, post internal.Post) (internal.Post, error) {
 
 	err := r.Conn.QueryRow(
 		ctx,
@@ -36,10 +34,7 @@ func (r *RepositoryPostgres) Insert(post internal.Post) (internal.Post, error) {
 	return post, nil
 }
 
-func (r *RepositoryPostgres) Delete(id uuid.UUID) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
+func (r *RepositoryPostgres) Delete(ctx context.Context, id string) error {
 	tag, err := r.Conn.Exec(
 		ctx,
 		"DELETE FROM posts WHERE id = $1",
@@ -52,9 +47,54 @@ func (r *RepositoryPostgres) Delete(id uuid.UUID) error {
 	return err
 }
 
-func (r *RepositoryPostgres) FindOneByID(id uuid.UUID) (internal.Post, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+func (r *RepositoryPostgres) FindAll(ctx context.Context) ([]*internal.Post, error) {
+	rows, err := r.Conn.Query(
+		ctx,
+		"SELECT id, username, body, created_at FROM posts",
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var items []*internal.Post
+
+	for rows.Next() {
+		var item internal.Post
+		if err := rows.Scan(&item.ID, &item.Username, &item.Body, &item.CreatedAt); err != nil {
+			// fmt.Println(err)
+			return nil, err
+		}
+
+		items = append(items, &item)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return items, nil
+}
+
+func (r *RepositoryPostgres) Update(ctx context.Context, post internal.Post) error {
+	_, err := r.Conn.Exec(
+		ctx,
+		"update post SET username=$1, body=$2 where id=$3",
+		post.Username,
+		post.Body,
+		post.ID,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *RepositoryPostgres) FindOneByID(ctx context.Context, id string) (internal.Post, error) {
 
 	var post internal.Post
 	err := r.Conn.QueryRow(
--- internal/post/service.go
+++ internal/post/service.go
@@ -1,37 +1,67 @@
 package post
 
 import (
+	"context"
 	"errors"
 	"unicode/utf8"
 
 	"github.com/filhodanuvem/ytgoapi/internal"
-	"github.com/google/uuid"
 )
 
 var ErrPostBodyEmpty = errors.New("post body is empty")
 var ErrPostBodyExceedsLimit = errors.New("post body exceeds limit")
 var ErrPostNotFound = errors.New("post not found")
+var ErrPostUsernameEmpty = errors.New("post username empty")
+var ErrIdEmpty = errors.New("id empty")
 
 type Service struct {
 	Repository Repository
 }
 
-func (p Service) Create(post internal.Post) (internal.Post, error) {
+func (p Service) Create(ctx context.Context, post internal.Post) (internal.Post, error) {
 	if post.Body == "" {
 		return internal.Post{}, ErrPostBodyEmpty
+	}
+
+	if post.Username == "" {
+		return internal.Post{}, ErrPostUsernameEmpty
 	}
 
 	if utf8.RuneCountInString(post.Body) > 140 {
 		return internal.Post{}, ErrPostBodyExceedsLimit
 	}
 
-	return p.Repository.Insert(post)
+	return p.Repository.Insert(ctx, post)
 }
 
-func (s Service) Delete(id uuid.UUID) error {
-	return s.Repository.Delete(id)
+func (s Service) Delete(ctx context.Context, id string) error {
+	return s.Repository.Delete(ctx, id)
 }
 
-func (s Service) FindOneByID(id uuid.UUID) (internal.Post, error) {
-	return s.Repository.FindOneByID(id)
+func (s Service) FindOneByID(ctx context.Context, id string) (internal.Post, error) {
+	return s.Repository.FindOneByID(ctx, id)
+}
+
+func (s Service) FindAll(ctx context.Context) ([]*internal.Post, error) {
+	posts, err := s.Repository.FindAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return posts, nil
+}
+
+func (s Service) Update(ctx context.Context, params *internal.ParamsUpdatePost) error {
+	post, err := params.Validate()
+	if err != nil {
+		return err
+	}
+
+	err = s.Repository.Update(ctx, post)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
